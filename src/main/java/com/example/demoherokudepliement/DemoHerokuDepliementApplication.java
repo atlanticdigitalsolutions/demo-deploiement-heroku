c4378fb20d1a549f2dfbb0d0ef9924b206e51dd8
Papa Matar Dieng <papamatar.dieng@atlanticdigitalsolutions-sn.com>
2023-11-21 15:31:41 +0000

    first commit

---
+++ src/main/java/com/example/demoherokudepliement/DemoHerokuDepliementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoherokudepliement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuDepliementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHerokuDepliementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoherokudepliement/DemoHerokuDepliementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoherokudepliement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHerokuDepliementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
