6b55140aa41bff2425abf957dfcf6a0f3527095c
Papa Matar Dieng <papamatar.dieng@atlanticdigitalsolutions-sn.com>
2023-11-22 13:34:38 +0000

    Correction cors configuration

--- src/main/java/com/example/demoherokudepliement/CorsConfig.java
+++ src/main/java/com/example/demoherokudepliement/CorsConfig.java
@@ -1,9 +1,11 @@
 package com.example.demoherokudepliement;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
 
     @Bean
